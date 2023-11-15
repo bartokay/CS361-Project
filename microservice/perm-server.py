f47277dc89cc65e1d9d6171cdf8158e6ff15dee1
kayla <bartokay@oregonstate.edu>
2023-11-15 01:20:25 -0800

    cleaned up for pretties. done!

--- microservice/perm-server.py
+++ microservice/perm-server.py
@@ -9,38 +9,46 @@ from urllib.parse import urlparse, parse_qs
 import re
 
 class MyServer(http.server.BaseHTTPRequestHandler):
-    def permutations(self, min, max):
+    def _permutations(self, min, max):
+        """
+        returns a random permutation of the numbers between min and max
+        """
         nums = list(range(min, max + 1))
         random.shuffle(nums)
         return nums
 
+    def _send_response(self, code, json_obj):
+        self.send_response(code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(bytes(json_obj, 'utf-8'))
+
+    def _validate_min_max(self, min, max):
+        if min > max:
+            raise Exception("min less than max")
+        if min < 0 or max > 1000:
+            raise Exception("min max range: 0-1000")
+        
     def do_GET(self):
         
+        # validate path: /perm/?min=1&max=4
         if re.search(r"/perm/\?min=[0-9]+&max=[0-9]+", self.path):
+
             try:
                 min_max = parse_qs(urlparse(self.path).query)
-                min = int(min_max["min"][0])
-                max = int(min_max["max"][0])
-                if min > max:
-                    error = "min less than max"
-                    raise Exception
-                if min < 0 or max > 1000:
-                    error = "min max range: 0-1000"
-                    raise Exception
-                perm = self.permutations(min, max)
+                min, max = int(min_max["min"][0]), int(min_max["max"][0])
+                self._validate_min_max(min, max)
 
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(bytes(json.dumps({'perm': perm}), 'utf-8'))
-            except:    
-                self.send_response(400)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(bytes(json.dumps({'error_message': error}), 'utf-8'))
+                # get permutation
+                perm = self._permutations(min, max)
+
+                # send response with permutation
+                self._send_response(200, json.dumps({'perm': perm}))
+                
+            except Exception as err:    
+                self._send_response(400, json.dumps({'error_message': str(err)}))
         else:
-            self.send_response(404)
-            self.wfile.write(bytes(json.dumps({'URL': 'Not Found'}), 'utf-8'))
+            self._send_response(404, json.dumps({'error_message': 'URL Not Found'}))
 
 PORT = 8000
 
