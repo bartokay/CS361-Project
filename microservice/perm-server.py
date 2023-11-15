f4c4cd0256053905d5f3ca5543654928875c889c
kayla <bartokay@oregonstate.edu>
2023-11-15 00:57:09 -0800

    added error checking and path validation

--- microservice/perm-server.py
+++ microservice/perm-server.py
@@ -1,10 +1,12 @@
-# citation: https://docs.python.org/3/library/http.server.html#http.server.SimpleHTTPRequestHandler
+# citations: https://docs.python.org/3/library/http.server.html#http.server.SimpleHTTPRequestHandler
+#            https://itecnote.com/tecnote/python-parse-http-get-and-post-parameters-from-basehttphandler/
 
 import http.server
 import socketserver
 import json
 import random
 from urllib.parse import urlparse, parse_qs
+import re
 
 class MyServer(http.server.BaseHTTPRequestHandler):
     def permutations(self, min, max):
@@ -13,17 +15,32 @@ class MyServer(http.server.BaseHTTPRequestHandler):
         return nums
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+        
+        if re.search(r"/perm/\?min=[0-9]+&max=[0-9]+", self.path):
+            try:
+                min_max = parse_qs(urlparse(self.path).query)
+                min = int(min_max["min"][0])
+                max = int(min_max["max"][0])
+                if min > max:
+                    error = "min less than max"
+                    raise Exception
+                if min < 0 or max > 1000:
+                    error = "min max range: 0-1000"
+                    raise Exception
+                perm = self.permutations(min, max)
 
-        min_max = parse_qs(urlparse(self.path).query)
-        min = int(min_max["min"][0])
-        max = int(min_max["max"][0])
-        perm = self.permutations(min, max)
-
-        self.wfile.write(bytes(json.dumps({'perm': perm}), 'utf-8'))
-
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps({'perm': perm}), 'utf-8'))
+            except:    
+                self.send_response(400)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps({'error_message': error}), 'utf-8'))
+        else:
+            self.send_response(404)
+            self.wfile.write(bytes(json.dumps({'URL': 'Not Found'}), 'utf-8'))
 
 PORT = 8000
 
