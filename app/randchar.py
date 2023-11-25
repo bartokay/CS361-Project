d014a053ccfa7e55862294b3a54202e9218be376
kayla <bartokay@oregonstate.edu>
2023-11-25 00:08:31 -0800

    renamed

--- app/randchar.py
+++ app/randchar.py
@@ -1,6 +1,9 @@
 import time
 
 def get_rand_id():
+    """
+    get random character id number from random_character_gen.py
+    """
     rand_char = open('C:/Users/b/Desktop/CS361-Project/char-id-gen/random_character_gen.txt', 'w')
     rand_char.write("run")
     rand_char.close()
